bf56b1bd362f47d8f94e8a3ef4cdcccd1e28fd4e
saniyaansari25 <saniyaansari251998@gmail.com>
2023-01-11 11:18:31 +0530

    Initial Commit

---
+++ dataNode.js
@@ -0,0 +1,8 @@
+
+let students=[
+    {id:1, name: "Jack", course: "React", role: "admin"},
+    {id:2, name: "Steve", course: "Angular"},
+    {id:3, name: "Anna", course: "JavaScript"},
+    {id:4, name: "Sam", course: "CSS"},
+]
+module.exports.students=students;

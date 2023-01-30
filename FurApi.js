a3230875a4706fc133e30b6709cee45791bee620
saniyaansari25 <saniyaansari251998@gmail.com>
2023-01-30 17:45:48 +0530

    Initial Commit

---
+++ FurApi.js
@@ -0,0 +1,222 @@
+let express=require("express");
+let app=express();
+app.use(express.json());
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin","*");
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, PATCH, DELETE, HEAD"
+    );
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    next();
+})
+//const port=2410;
+var port=process.env.PORT || 2410;
+app.listen(port,()=>console.log(`Node app listening on port ${port}!`));
+
+let {data}=require("./furData.js");
+let cart=[];
+let addToCart=[];
+
+let fs=require("fs")
+let fname1="furniture.json";
+let fname2="cart.json";
+let fname3="add1.json";
+
+app.get("/",function(req,res){
+    let data1=JSON.stringify(data)
+    fs.writeFile(fname1,data1,function(err){
+        if(err)res.status(404).send(err)
+        fs.readFile(fname1,"utf8",function(err,data){
+            if(err)res.status(404).send(err)
+            else{
+                let furArray=JSON.parse(data)
+                res.send(furArray)
+            }
+        })
+    })
+})
+app.post("/products/add",function(req,res){
+    let body=req.body
+    console.log(body)
+    fs.readFile(fname1,"utf8",function(err,data){
+        if(err)res.status(404).send(err)
+        let newProduct={...body}
+        let a=[...JSON.parse(data)]
+        a.push(newProduct)
+        console.log(a)
+        let d1=JSON.stringify(a)
+        fs.writeFile(fname1,d1,function(err){
+            if(err)res.status(404).send(err)
+            else res.send("Done")
+        })
+    })     
+})
+app.put("/products/:prodCode",function(req,res){
+    let prodCode=req.params.prodCode
+    let body=req.body
+    fs.readFile(fname1,"utf8",function(err,data){
+        if(err)console.log(err)
+        else{
+            let furArray=JSON.parse(data)
+            let index=furArray.findIndex(n=>n.prodCode===prodCode)
+            furArray[index]={...body}
+            let d1=JSON.stringify(furArray)
+            fs.writeFile(fname1,d1,function(err){
+                if(err)res.status(404).send(err)
+                else res.send("Done")
+            })
+        }
+    })
+})
+app.delete("/products/:prodCode",function(req,res){
+    let prodCode=req.params.prodCode
+    fs.readFile(fname1,"utf8",function(err,data){
+        if(err)console.log(err)
+        else{
+            let furArray=JSON.parse(data)
+            let index=furArray.findIndex(n=>n.prodCode===prodCode)
+            furArray.splice(index,1)
+            let d1=JSON.stringify(furArray)
+            fs.writeFile(fname1,d1,function(err){
+                if(err)res.status(404).send(err)
+                else res.send("Done")
+            })
+        }
+    })
+})
+app.get("/products",function(req,res){
+    fs.readFile(fname1,"utf8",function(err,data){
+        if(err)res.status(404).send(err)
+        else{
+            let furArray=JSON.parse(data)
+            res.send(furArray)
+        }
+    })
+})
+
+app.get("/products/:prodCode",function(req,res){
+    let prodCode=req.params.prodCode
+    console.log(prodCode)
+    if(prodCode=="Dining"||prodCode=="Drawing"||prodCode=="Bedroom"|| prodCode=="Study")
+    {
+        fs.readFile(fname1,"utf8",function(err,data){
+            if(err)res.status(404).send(err)
+            else{
+                let furArray=JSON.parse(data)
+                let products=furArray.filter(n=>n.category==prodCode)
+                
+                res.send(products)
+            }
+        })
+    }
+    else if(prodCode)
+    {
+        fs.readFile(fname1,"utf8",function(err,data){
+            if(err)res.status(404).send(err)
+            else{
+                let productArray=JSON.parse(data)
+                let cart=productArray.find(n=>n.prodCode==prodCode)
+                let d=JSON.stringify(cart)
+                fs.writeFile(fname2,d,function(err){
+                    if(err)res.status(404).send(err)
+                    else{
+                        fs.readFile(fname1,"utf8",function(err,data){
+                            if(err)res.status(404).send(err)
+                            else{
+                                let furArray=JSON.parse(data)
+                                let products=furArray.filter(n=>n.category==cart.category)
+                                let j={cart:cart,products:products}
+                                res.send(j)
+                            }
+                        })
+                    }
+                })
+            }
+        })
+    }
+    else{
+        fs.readFile(fname1,"utf8",function(err,data){
+            if(err)res.status(404).send(err)
+            else{
+                let furArray=JSON.parse(data)
+                res.send(furArray)
+            }
+        })
+    }
+})
+
+
+
+app.post("/login",function(req,res){
+    let email=req.body.email
+    let password=req.body.password
+    if(email=="user@user.com" && password=="12345678")
+    {
+        fs.readFile(fname1,"utf8",function(err,data){
+            if(err)res.status(404).send(err)
+            else{
+                let furArray=JSON.parse(data)
+                res.send(furArray)
+            }
+        }) 
+    }
+    else if(email=="admin@admin.com" && password=="admin123")
+    {
+        fs.readFile(fname1,"utf8",function(err,data){
+            if(err)res.status(404).send(err)
+            else{
+                let furArray=JSON.parse(data)
+                res.send(furArray)
+            }
+        }) 
+    }
+})
+app.post("/cart",function(req,res){
+    let body=req.body
+    fs.readFile(fname3,"utf8",function(err,data){
+        if(err)res.status(404).send(err)
+        let newProduct={...body}
+        addToCart.find(n=>n==newProduct ? n.qty+=1 : '')
+        addToCart.push(newProduct)
+        let data1=JSON.stringify(addToCart)
+        fs.writeFile(fname3,data1,function(err){
+            if(err)res.status(404).send(err)
+            else res.send(addToCart)
+        })
+    })    
+})
+app.get("/cart",function(req,res){
+    fs.readFile(fname3,"utf8",function(err,data){
+        if(err)console.log(err)
+        else{
+           res.send(addToCart)
+        }
+    })
+})
+app.put("/cart/:prodCode",function(req,res){
+    let prodCode=req.params.prodCode
+    let body=req.body
+    fs.readFile(fname3,"utf8",function(err,data){
+        if(err)console.log(err)
+        else{
+            let index=addToCart.findIndex(n=>n.prodCode===prodCode)
+            addToCart[index]={...body}
+            res.send(addToCart)
+        }
+    })
+})
+app.delete("/cart/:prodCode",function(req,res){
+    let prodCode=req.params.prodCode
+    fs.readFile(fname3,"utf8",function(err,data){
+        if(err)console.log(err)
+        else{
+            let index=addToCart.findIndex(n=>n.prodCode===prodCode)
+            addToCart.splice(index,1)
+            res.send(addToCart)
+        }
+    })
+})
---
+++ furData.js
@@ -0,0 +1,175 @@
+let data=[
+    {
+        prodCode:"DS2S245",
+        category:"Dining",
+        desc:[
+                "Two seater	Dining Set",
+                "Built from	High quality wood",
+                "1 year warranty"
+            ],
+        img:"https://media.istockphoto.com/id/638857698/photo/table-and-two-chairs-on-white-background.jpg?s=612x612&w=is&k=20&c=MfGbKAaK95u1RydF76EnzVfSBipZLRbpE6NCPzn0vp8=",
+        ingredients:[
+            {ingName:"Dining Table",qty:1},
+            {ingName:"Chair",qty:2}
+        ],
+        title:"Two seater Dining Set"
+    },
+    {
+        prodCode:"DS6S761",
+        category:"Dining",
+        desc:[
+                "Six Seater	Dining Set in Antique Cherry Colour",
+                "Assembly by Skilled Carpenters",
+                "Made from Teak	wood"
+            ],
+        img:"https://hometown.gumlet.io/media/product/89/3163/57821/1.jpg?mode=fill&h=576&w=576&dpr=1.3",
+        ingredients:[
+                {ingName:"Dining Table",qty:1},
+                {ingName:"Chair",qty:4},
+                {ingName:"Bench",qty:1}
+        ],
+        title:"Six Seater Dining Set"
+    },
+    {
+        prodCode:"DS4S177",
+        category:"Dining",
+        desc:[
+                "Mild Steel	Four Seater	Dining Set in Black Colour",
+                "Knock-down	construction for easy transportation"
+            ],
+        img:"https://hometown.gumlet.io/media/product/08/2453/47088/1.jpg?mode=fill&h=576&w=576&dpr=1.3",
+        ingredients:[
+                {ingName:"Dining Table",qty:1},
+                {ingName:"Chair",qty:4}
+            ],
+        title:"Mild	Steel Dining Set"
+    },
+    {
+        prodCode:"DC2S705",
+        category:"Dining",
+        desc:[
+            "Solid Wood	Dining Chair Set of	Two	in Dark Walnut Colour",
+            "Beautiful design carved on	dining chair",
+            "Dining	chair seat upholstered in dark brown Fabric"
+        ],
+        img:"https://hometown.gumlet.io/media/product/61/9353/47156/1.jpg?mode=fill&h=576&w=576&dpr=1.3",
+        ingredients:[
+            {ingName:"Chair",qty:2}
+        ],
+        title:"Dining Chair	Set"
+    },
+    {
+        prodCode:"BN1S388",
+        category:"Dining",
+        desc:[
+            "Solid Wood	Dining Bench in	Dark Walnut	Colour",
+            "Comfortable bench for a relaxed dinner"
+        ],
+        img:"https://hometown.gumlet.io/media/product/31/9863/72274/1.jpg?mode=fill&h=576&w=576&dpr=1.3",
+        ingredients:[
+            {ingName:"Bench",qty:1}
+        ],
+        title:"Dining Bench"
+    },
+    {
+        prodCode:"SF2S532",
+        category:"Drawing",
+        desc:[
+            "Characteristic	Rising Track Arm Rest Design",
+            "Premium High Gloss	Leatherette	Upholstery",
+            "Independent Headrest And Lumber Support"
+        ],
+        img:"https://hometown.gumlet.io/media/product/25/8273/44943/1.jpg?mode=fill&w=335&h=335&dpr=1.3",
+        ingredients:[
+            {ingName:"Sofa",qty:1}
+        ],
+        title:"Two Seater Sofa"
+    },
+    {
+        prodCode:"SF2S206",
+        category:"Drawing",
+        desc:[
+            "Two Seater Sofa in Blue Colour",
+             "Assembly by Skilled Carpenters"
+        ],
+        img:"https://hometown.gumlet.io/media/product/15/8273/79687/1.jpg?mode=fill&h=576&w=576&dpr=1.3",
+        ingredients:[
+            {ingName:"Sofa",qty:1}
+        ],
+        title:"Two	Seater	Sofa"
+    },
+    {
+        prodCode:"SFBD311",
+        category:"Drawing",
+        desc:[
+            "Sofa Cum bed in Brown Colour",
+            "Ply-wood construction with	hand polished finish",
+            "Removable fabric cover	on best	quality	foam mattress",
+            "Throw cushions	and	bolsters come with the product"
+        ],
+        img:"https://images.unsplash.com/photo-1493663284031-b7e3aefcae8e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+        ingredients:[
+            {ingName:"Sofa",qty:1},	
+            {ingName:"Cushions",qty:2}
+        ],
+        title:"Sofa	cum	Bed"
+    },
+    {
+        prodCode:"BDQS381",
+		category:"Bedroom",
+        desc:[
+            "Wood Box Storage King Size	Bed	in Wenge Colour	",
+            "Box Storage included for Maximum space	utilization",
+            "Mattress is included"
+        ],
+        img:"https://images.unsplash.com/photo-1505693416388-ac5ce068fe85?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+        ingredients:[
+            {ingName:"Bed",qty:1},
+        	{ingName:"Mattress",qty:2}
+        ],
+        title:"King	size Bed"
+    },
+    {
+        prodCode:"BDQS229",
+        category:"Bedroom",
+        desc:[
+            "Wood Hydraulic	Storage	Queen Size Bed",
+            "Half hydraulic	storage",
+            "Superior E2 grade MDF used	with melamine finish"
+        ],
+        img:"https://hometown.gumlet.io/media/product/43/8153/54285/1.jpg?mode=fill&h=576&w=576&dpr=1.3",
+        ingredients:[
+            {ingName:"Bed",qty:1}
+        ],
+        title:"Queen size Bed"
+    },
+    {
+        prodCode:"ST1T425",
+        category:"Study",
+        desc:[
+            "Wood Study	Table in Walnut	Colour",
+            "Assembly by Skilled Carpenters",
+            "Built from	High Quality Wood"
+        ],
+        img:"https://hometown.gumlet.io/media/product/42/8463/69263/1.jpg?mode=fill&h=576&w=576&dpr=1.3",
+        ingredients:[
+            {ingName:"Study	Table",qty:1}
+        ],
+        title:"Study Table"
+    },
+    {
+        prodCode:"ST1T588",
+        category:"Study",
+        desc:[
+            "Wood Study	Table in Highgloss White & Blue	Colour",
+            "Study table comes with	bookshelf on top, 5	drawers	& 1	open shelf",
+            "Superior quality MDF with stain resistant melamine	finish"
+        ],
+        img:"https://hometown.gumlet.io/media/product/26/4673/10215/1.jpg?mode=fill&h=576&w=576&dpr=1.3",
+        ingredients:[
+            {ingName:"Study	Table",qty:1}
+        ],
+        title:"Study Table"
+    }
+];
+module.exports.data=data;

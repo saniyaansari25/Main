a6dddb314f54af66356c8cd931a7eb71bcce9485
saniyaansari25 <saniyaansari251998@gmail.com>
2023-05-05 12:22:13 +0530

    Initial Commit

--- authService.js
+++ authService.js
@@ -1,6 +1,7 @@
 const keyName="user"
 function login(obj){
     let str=JSON.stringify(obj)
+    console.log("STR",str)
     localStorage.setItem(keyName, str)
 }
 function logout(){
@@ -16,3 +17,22 @@ export default{
     logout,
     getUser,
 }
+
+/*const keyName="user"
+function login(obj){
+    let str=JSON.stringify(obj)
+    localStorage.setItem(keyName, str)
+}
+function logout(){
+    localStorage.removeItem(keyName)
+}
+function getUser(){
+    let str=localStorage.getItem(keyName)
+    let obj=str ? JSON.parse(str) : null
+    return obj
+}
+export default{
+    login,
+    logout,
+    getUser,
+}*/
